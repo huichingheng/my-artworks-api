d059373ec77ffc3976fa8f9bb677a5d18d0d0ee9
huichingheng <huichingheng@gmail.com>
2018-07-16 12:20:46 +0800

    add config.yml

---
+++ tests/app.test.js
@@ -0,0 +1,7 @@
+const request = require("supertest");
+const app = require("../app");
+
+test("Get return Hello World ", async () => {
+  const response = await request(app).get("/");
+  expect(response.body).toEqual("Hello World");
+});
