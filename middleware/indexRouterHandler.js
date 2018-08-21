7285f9155c5b8f5bc1a50008e2704936c13584f8
huichingheng <huichingheng@gmail.com>
2018-08-21 19:03:48 +0800

    updated landing page route message to show documentation route

--- middleware/indexRouterHandler.js
+++ middleware/indexRouterHandler.js
@@ -1,41 +1,48 @@
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 const { jwtOptions } = require("../config/passport");
-const {userIsValid, userNotFound} = require('../middleware/indexSignUpUtility')
-
+const {
+  userIsValid,
+  userNotFound
+} = require("../middleware/indexSignUpUtility");
 
 const landingPage = (req, res) => {
-    res.json("use /artworks as endpoint");
-}
+  res.json({
+    message: "use /artworks as endpoint",
+    help: "use /api-docs as endpoint for documentation"
+  });
+};
 
 const signUp = async (req, res, next) => {
-    const { username, password } = req.body;
-    const user = new User({ username, bio: "some bio" });
-    user.setHashedPassword(password);
-    try {
-        await user.save();
-        res.json({ user });
-    } catch (err) {
-        next(err);
-    }
-}
+  const { username, password } = req.body;
+  const user = new User({ username, bio: "some bio" });
+  user.setHashedPassword(password);
+  try {
+    await user.save();
+    res.json({ user });
+  } catch (err) {
+    next(err);
+  }
+};
 
 const signIn = async (req, res) => {
-    const { username, password } = req.body;
+  const { username, password } = req.body;
 
-    const user = await User.findOne({ username });
+  const user = await User.findOne({ username });
 
-    if (!user) {userNotFound(res)}
+  if (!user) {
+    userNotFound(res);
+  }
 
-    if (user.validatePassword(password)) {
-        userIsValid(user, jwt, jwtOptions, res)
-    } else {
-        userIsNotValid()
-    }
-}
+  if (user.validatePassword(password)) {
+    userIsValid(user, jwt, jwtOptions, res);
+  } else {
+    userIsNotValid();
+  }
+};
 
 module.exports = {
-    landingPage,
-    signUp,
-    signIn
-}
+  landingPage,
+  signUp,
+  signIn
+};
